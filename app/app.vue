43fd7adc7c9eda003d605802c0db1201f0f3d890
Guillermo Castano <guillermo.c.velez@outlook.com>
2026-07-29 19:08:21 -0500

    Feature - Actualización de estilos

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="av-bg"></div>
+    <div class="av-noise"></div>
+    <div id="root">
+      <NuxtRouteAnnouncer />
+      <main class="av-main">
+        <div class="av-hero">
+          <h1 class="pixel flicker">Arcade Vault</h1>
+          <p class="sub"><span class="neon-yellow">Insert coin</span> <span class="blink">_</span></p>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
